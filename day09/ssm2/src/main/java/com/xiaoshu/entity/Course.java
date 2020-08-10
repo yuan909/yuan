29fd8a11a4510f39dcf6c5095098a6c22b8a9457
yuan <2173378960@qq.com>
2020-08-10 20:29:49 +0800

    提交day09作业

---
+++ day09/ssm2/src/main/java/com/xiaoshu/entity/Course.java
@@ -0,0 +1,88 @@
+package com.xiaoshu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+public class Course implements Serializable {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String code;
+
+    private Date createtime;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    /**
+     * @return code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code
+     */
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    /**
+     * @return createtime
+     */
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    /**
+     * @param createtime
+     */
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", code=").append(code);
+        sb.append(", createtime=").append(createtime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
