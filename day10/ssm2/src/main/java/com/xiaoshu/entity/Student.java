2fcd2ed1060d958e8d680ca64c9f7e8a6bedfb3f
yuan <2173378960@qq.com>
2020-08-11 20:52:30 +0800

    提交day10作业

---
+++ day10/ssm2/src/main/java/com/xiaoshu/entity/Student.java
@@ -0,0 +1,124 @@
+package com.xiaoshu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Student implements Serializable {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String age;
+
+    private Integer courseid;
+
+    private String grade;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date entrytime;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    /**
+     * @return age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(String age) {
+        this.age = age == null ? null : age.trim();
+    }
+
+    /**
+     * @return courseid
+     */
+    public Integer getCourseid() {
+        return courseid;
+    }
+
+    /**
+     * @param courseid
+     */
+    public void setCourseid(Integer courseid) {
+        this.courseid = courseid;
+    }
+
+    /**
+     * @return grade
+     */
+    public String getGrade() {
+        return grade;
+    }
+
+    /**
+     * @param grade
+     */
+    public void setGrade(String grade) {
+        this.grade = grade == null ? null : grade.trim();
+    }
+
+    /**
+     * @return entrytime
+     */
+    public Date getEntrytime() {
+        return entrytime;
+    }
+
+    /**
+     * @param entrytime
+     */
+    public void setEntrytime(Date entrytime) {
+        this.entrytime = entrytime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append(", courseid=").append(courseid);
+        sb.append(", grade=").append(grade);
+        sb.append(", entrytime=").append(entrytime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
