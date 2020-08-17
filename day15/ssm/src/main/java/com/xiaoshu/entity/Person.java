2185f79a6628f5ccaebca4fe22f62dbaab5d355e
yuan <2173378960@qq.com>
2020-08-17 20:44:40 +0800

    提交day15作业

---
+++ day15/ssm/src/main/java/com/xiaoshu/entity/Person.java
@@ -0,0 +1,167 @@
+package com.xiaoshu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Table(name = "jiyun_person")
+public class Person implements Serializable {
+    @Id
+    @Column(name = "p_id")
+    private Integer pId;
+
+    @Column(name = "p_name")
+    private String pName;
+
+    @Column(name = "p_sex")
+    private String pSex;
+
+    @Column(name = "p_like")
+    private String pLike;
+
+    @Column(name = "p_count")
+    private String pCount;
+
+    @Column(name = "p_age")
+    private String pAge;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createtime;
+
+    @Column(name = "b_id")
+    private Integer bId;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return p_id
+     */
+    public Integer getpId() {
+        return pId;
+    }
+
+    /**
+     * @param pId
+     */
+    public void setpId(Integer pId) {
+        this.pId = pId;
+    }
+
+    /**
+     * @return p_name
+     */
+    public String getpName() {
+        return pName;
+    }
+
+    /**
+     * @param pName
+     */
+    public void setpName(String pName) {
+        this.pName = pName == null ? null : pName.trim();
+    }
+
+    /**
+     * @return p_sex
+     */
+    public String getpSex() {
+        return pSex;
+    }
+
+    /**
+     * @param pSex
+     */
+    public void setpSex(String pSex) {
+        this.pSex = pSex == null ? null : pSex.trim();
+    }
+
+    /**
+     * @return p_like
+     */
+    public String getpLike() {
+        return pLike;
+    }
+
+    /**
+     * @param pLike
+     */
+    public void setpLike(String pLike) {
+        this.pLike = pLike == null ? null : pLike.trim();
+    }
+
+    /**
+     * @return p_count
+     */
+    public String getpCount() {
+        return pCount;
+    }
+
+    /**
+     * @param pCount
+     */
+    public void setpCount(String pCount) {
+        this.pCount = pCount == null ? null : pCount.trim();
+    }
+
+    /**
+     * @return p_age
+     */
+    public String getpAge() {
+        return pAge;
+    }
+
+    /**
+     * @param pAge
+     */
+    public void setpAge(String pAge) {
+        this.pAge = pAge == null ? null : pAge.trim();
+    }
+
+    /**
+     * @return createtime
+     */
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    /**
+     * @param createtime
+     */
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    /**
+     * @return b_id
+     */
+    public Integer getbId() {
+        return bId;
+    }
+
+    /**
+     * @param bId
+     */
+    public void setbId(Integer bId) {
+        this.bId = bId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", pId=").append(pId);
+        sb.append(", pName=").append(pName);
+        sb.append(", pSex=").append(pSex);
+        sb.append(", pLike=").append(pLike);
+        sb.append(", pCount=").append(pCount);
+        sb.append(", pAge=").append(pAge);
+        sb.append(", createtime=").append(createtime);
+        sb.append(", bId=").append(bId);
+        sb.append("]");
+        return sb.toString();
+    }
+}
